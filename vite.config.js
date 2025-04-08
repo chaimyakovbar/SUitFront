459476b1991f33b6234f9d871182e76ea120cdf5
chim Yakov Barzesky <chaimyakovbar@gmail.com>
2025-04-08 16:50:59 +0300

    git remove nodemoudles V2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
   plugins: [react()],
-});
+  base: '/',
+})
